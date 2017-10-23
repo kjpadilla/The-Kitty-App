ba55d86d568635a89cb3aa7abee0bfbd77c12305
Kristofer Padilla <kristofer@Kristofers-iMac.local>
2017-10-23 00:57:54 -0700

    Initial Commit

---
+++ The Kitty App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Kitty App
+//
+//  Created by Kristofer Padilla on 10/23/17.
+//  Copyright © 2017 Kristofer Padilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
