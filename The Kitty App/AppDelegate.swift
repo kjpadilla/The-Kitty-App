28d3a88ef3bcfd7a1c516711d4f49db52fc2cc0f
kjpadilla <kjpadilla@ucdavis.edu>
2017-11-05 01:59:35 -0700

    collectionView started.

--- The Kitty App/AppDelegate.swift
+++ The Kitty App/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        let rulesCollectionViewController = RulesCollectionViewController(collectionViewLayout: flowLayout)
+        
+        window?.rootViewController = UINavigationController(rootViewController: rulesCollectionViewController)
+        
         // Override point for customization after application launch.
         return true
     }
--- The Kitty App/ViewController.swift
+++ The Kitty App/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RulesCollectionViewController: UICollectionViewController	{
 
+    let customCellIdentifier = "customCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.blue
+        
+        collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customCellIdentifier)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath)
+        
+        return customCell
     }
 
 
+}
+
+class CustomCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Custom Text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setUpViews() {
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
