3f0ccaccc14e41f2fbdcdcd8219e79487ea1d4d4
kjpadilla <kjpadilla@ucdavis.edu>
2017-11-07 04:42:59 -0800

    coloring and basic layout

--- The Kitty App/ViewController.swift
+++ The Kitty App/ViewController.swift
@@ -1,4 +1,4 @@
-//
+	//
 //  ViewController.swift
 //  The Kitty App
 //
@@ -8,28 +8,49 @@
 
 import UIKit
 
-class RulesCollectionViewController: UICollectionViewController	{
+class RulesCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout	{
 
     let customCellIdentifier = "customCellIdentifier"
+    let theRules = [
+    "Rule 1",
+    "Rule 2",
+    "Rule 3",
+    "Rule 1",
+    "Rule 2",
+    "Rule 3",
+    "Rule 1",
+    "Rule 2",
+    "Rule 3",
+    "Rule 1",
+    "Rule 2",
+    "Rule 3"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = UIColor.blue
+        navigationItem.title = "The Rules"
+        
+        collectionView?.backgroundColor = UIColor(red: 252/255, green: 237/255, blue: 244/255, alpha: 1)
         
         collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customCellIdentifier)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 12
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath)
+        customCell.layer.cornerRadius = 6
         
         return customCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 60)
     }
 
 
@@ -44,15 +65,32 @@ class CustomCell: UICollectionViewCell {
     
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "Custom Text"
+        
+        label.backgroundColor = UIColor(red: 252/255, green: 237/255, blue: 244/255, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.sizeToFit()
+        label.numberOfLines = 0
+        label.text = "Custom Text Custom Text Custom Text Custom Text Custom Text Custom Text Custom Text"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    let completedRuleButton: UIButton = {
+        let checkButton = UIButton()
+        checkButton.backgroundColor = UIColor(red: 252/255, green: 237/255, blue: 244/255, alpha: 1)
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        return checkButton
+    }()
+    
     func setUpViews() {
+        backgroundColor = UIColor(red: 249/255, green: 199/255, blue: 224/255, alpha: 1)
+        
         addSubview(nameLabel)
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addSubview(completedRuleButton)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-4-[v0]-[v1(44)]-4-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": completedRuleButton]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(44)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(44)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": completedRuleButton]))
         
     }
     
